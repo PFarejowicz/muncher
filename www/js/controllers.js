f20853f47ab4ebab182c870e18fca08c64cf7821
PFarejowicz <pfarejowicz@gmail.com>
2015-12-03 00:07:34 -0800

    finished with app

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -53,23 +53,6 @@ angular.module('starter.controllers', [])
 
   var genieIndex = 0;
   var pollIndex = 0;
-  var genieText = ["Hi! I’m Muncher, and I’m here to help you decide what to eat. Launch polls and decide places now with the light-bulb button at the bottom left!",
-    "Awesome! The votes are out:\n Japanese - 4, Italian - 1",
-    "Awesome! The votes are out:\n Palo Alto - 3, Mountain View - 2",
-    "Jin Sho - Modern sushi bar and Japanese kitchen",
-    "Rangoon Ruby - Hearty Burmese family-style cuisine",
-    "Nola's - New Orleans soul food, jazz and drinks",
-    "St. Michael's Alley - New American modern brunch and coffee",
-    "La Bodeguita de Medio - Cuban fine dining with a Latin flair",
-    "Excellent! Based on your group’s preferences, here are the top 3 choices. Click and drag to rank!\n 1. Nola's\n 2. St. Michael's Alley\n 3. Rangoon Ruby",
-    "Are you sure?",
-    "Based on everyone’s preferences, I’ve decided on this place for you all:",
-    "St. Michael's Alley - New American modern brunch and coffee",
-    "I’ve made reservations for St. Michael’s Alley on: Sun Nov 1, 11:30am - 1:30pm",
-    "I’ve added it into your calendar! You’re good to go."];
-  var pollText = ["What cuisine do you prefer at the moment?",
-    "What neighborhood do you prefer at the moment?",
-    "I have a few places in mind: indicate Yes or No and I'll shortlist 3 top options."];
 
   var isIOS = ionic.Platform.isWebView() && ionic.Platform.isIOS();
 
@@ -77,132 +60,134 @@ angular.module('starter.controllers', [])
     var d = new Date();
     d = d.toLocaleTimeString().replace(/:\d+ /, ' ');
 
-    $scope.messages.push({
-      userId: '54321',
-      text: $scope.data.message,
-      time: d,
-      width: "0",
-      height: "0"
-    });
+    if ($scope.data.message != null) {
+      $scope.messages.push({
+        userId: '54321',
+        text: $scope.data.message,
+        time: d,
+        width: "0",
+        height: "0"
+      });
 
-    if (genieIndex == 0) {
-      $scope.messages.push({
-        userId: '12345',
-        time: d,
-        img: "img/1.png",
-        width: "210",
-        height: "120"
-      });
-      genieIndex++;
-      pollIndex++;
-    } else if (genieIndex == 2) {
-      $scope.messages.push({
-        userId: '12345',
-        time: d,
-        img: "img/3.png",
-        width: "170",
-        height: "80"
-      });
-      genieIndex++;
-      pollIndex++;
-    } else if (genieIndex == 4) {
-      $scope.messages.push({
-        userId: '12345',
-        time: d,
-        img: "img/5.png",
-        width: "170",
-        height: "100"
-      });
-      genieIndex++;
-      pollIndex++;
-    } else if (genieIndex == 7) {
-      $scope.messages.push({
-        userId: '12345',
-        time: d,
-        img: "img/8.png",
-        width: "200",
-        height: "142"
-      });
-      genieIndex++;
-    } else if (genieIndex == 8) {
-      $scope.messages.push({
-        userId: '12345',
-        time: d,
-        img: "img/9.png",
-        width: "200",
-        height: "142"
-      });
-      genieIndex++;
-    } else if (genieIndex == 9) {
-      $scope.messages.push({
-        userId: '12345',
-        time: d,
-        img: "img/10.png",
-        width: "200",
-        height: "142"
-      });
-      genieIndex++;
-    } else if (genieIndex == 10) {
-      $scope.messages.push({
-        userId: '12345',
-        time: d,
-        img: "img/11.png",
-        width: "200",
-        height: "142"
-      });
-      genieIndex++;
-    } else if (genieIndex == 11) {
-      $scope.messages.push({
-        userId: '12345',
-        time: d,
-        img: "img/12.png",
-        width: "200",
-        height: "310"
-      });
-      genieIndex++;
-    } else if (genieIndex == 12) {
-      $scope.messages.push({
-        userId: '12345',
-        time: d,
-        img: "img/13.png",
-        width: "90",
-        height: "30"
-      });
-      genieIndex++;
-    } else if (genieIndex == 13) {
-      $scope.messages.push({
-        userId: '12345',
-        time: d,
-        img: "img/14.png",
-        width: "200",
-        height: "70"
-      });
-      $scope.messages.push({
-        userId: '12345',
-        time: d,
-        img: "img/15.png",
-        width: "200",
-        height: "160"
-      });
-      genieIndex++;
-    } else if (genieIndex == 14) {
-      $scope.messages.push({
-        userId: '12345',
-        time: d,
-        img: "img/16.png",
-        width: "200",
-        height: "110"
-      });
-      genieIndex++;
-    } else if (genieIndex == 15) {
-      $scope.messages.push({
-        userId: '12345',
-        time: d,
-        img: "img/17.png",
-        width: "200",
-        height: "50"
-      });
-      genieIndex++;
+      if (genieIndex == 0) {
+        $scope.messages.push({
+          userId: '12345',
+          time: d,
+          img: "img/1.png",
+          width: "210",
+          height: "120"
+        });
+        genieIndex++;
+        pollIndex++;
+      } else if (genieIndex == 2) {
+        $scope.messages.push({
+          userId: '12345',
+          time: d,
+          img: "img/3.png",
+          width: "170",
+          height: "80"
+        });
+        genieIndex++;
+        pollIndex++;
+      } else if (genieIndex == 4) {
+        $scope.messages.push({
+          userId: '12345',
+          time: d,
+          img: "img/5.png",
+          width: "170",
+          height: "100"
+        });
+        genieIndex++;
+        pollIndex++;
+      } else if (genieIndex == 7) {
+        $scope.messages.push({
+          userId: '12345',
+          time: d,
+          img: "img/8.png",
+          width: "200",
+          height: "142"
+        });
+        genieIndex++;
+      } else if (genieIndex == 8) {
+        $scope.messages.push({
+          userId: '12345',
+          time: d,
+          img: "img/9.png",
+          width: "200",
+          height: "142"
+        });
+        genieIndex++;
+      } else if (genieIndex == 9) {
+        $scope.messages.push({
+          userId: '12345',
+          time: d,
+          img: "img/10.png",
+          width: "200",
+          height: "142"
+        });
+        genieIndex++;
+      } else if (genieIndex == 10) {
+        $scope.messages.push({
+          userId: '12345',
+          time: d,
+          img: "img/11.png",
+          width: "200",
+          height: "142"
+        });
+        genieIndex++;
+      } else if (genieIndex == 11) {
+        $scope.messages.push({
+          userId: '12345',
+          time: d,
+          img: "img/12.png",
+          width: "200",
+          height: "310"
+        });
+        genieIndex++;
+      } else if (genieIndex == 12) {
+        $scope.messages.push({
+          userId: '12345',
+          time: d,
+          img: "img/13.png",
+          width: "90",
+          height: "30"
+        });
+        genieIndex++;
+      } else if (genieIndex == 13) {
+        $scope.messages.push({
+          userId: '12345',
+          time: d,
+          img: "img/14.png",
+          width: "200",
+          height: "70"
+        });
+        $scope.messages.push({
+          userId: '12345',
+          time: d,
+          img: "img/15.png",
+          width: "200",
+          height: "160"
+        });
+        genieIndex++;
+      } else if (genieIndex == 14) {
+        $scope.messages.push({
+          userId: '12345',
+          time: d,
+          img: "img/16.png",
+          width: "200",
+          height: "110"
+        });
+        genieIndex++;
+      } else if (genieIndex == 15) {
+        $scope.messages.push({
+          userId: '12345',
+          time: d,
+          img: "img/17.png",
+          width: "200",
+          height: "50"
+        });
+        genieIndex++;
+      }
     }
 
     delete $scope.data.message;
